a1d7691dc6d62c6b40260cb3a3a2d361fb14dd3d
Atiksh Srivastava <31950686+Atiksh04@users.noreply.github.com>
2024-04-11 14:34:31 +0530

    added: basic changes

---
+++ app/llm_handler/llm_config.py
@@ -0,0 +1,33 @@
+import os
+
+from autogen import AssistantAgent, UserProxyAgent
+
+
+llm_config = {
+    "config_list": [
+        {
+            "model": "gpt-4-1106-preview",
+            "api_key": os.environ.get(
+                "OPENAI_SECERET", "sk-123131****"
+            ),
+        }
+    ]
+}
+
+super_agent = AssistantAgent(
+    "chatbot",
+    system_message="""You are a super intelligent personal assistant.
+    You have been given a set of tools that you are supposed to choose from.
+    You decide the right tool and execute it to achieve your task.
+    Reply TERMINATE when the task is done or when user's content is empty""",
+    llm_config=llm_config,
+)
+
+
+user_proxy = UserProxyAgent(
+    "user_proxy",
+    is_termination_msg=lambda x: x.get("content", "")
+    and "TERMINATE" in x.get("content", ""),
+    human_input_mode="NEVER", 
+    code_execution_config={"use_docker": False},
+)
--- run.py
+++ run.py
@@ -1,11 +1,19 @@
-from flask import Flask
-from app.routes import hello_route
+from flask import Flask, request
+from app.routes import hello_route, handle_trigger_endpoint
+from composio import Composio
+
+client = Composio("9qi05clkeqxtudolljveh") 
 
 app = Flask(__name__)
 
 @app.route('/api/hello', methods=['GET'])
 def get_hello():
     return hello_route()
+
+@app.route('/webhook/trigger', methods=['POST'])
+def handler_trigger():
+    return handle_trigger_endpoint()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
